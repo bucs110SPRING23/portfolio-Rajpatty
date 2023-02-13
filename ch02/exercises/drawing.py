bfc16a5ba229a0a6d82b60ff5d070e5994a7f7e8
syamrajpatty <srajpat1@binghamton.edu>
2023-02-13 16:51:45 -0500

    hello

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -5,9 +5,9 @@ beta = turtle.Turtle()
 beta.color("blue")
 
 sides = int(input("How many sides?"))
+length = int(input("How long is each side?"))
 
 for i in range(sides):
-    length = int(input("How long is each side?"))
 
     beta.forward(length)
     beta.right(360 / sides)
