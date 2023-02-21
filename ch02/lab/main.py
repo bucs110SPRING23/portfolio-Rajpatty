0d6b43d82dc25ff4a57f6146a026ba10c302f8e9
syamrajpatty <srajpat1@binghamton.edu>
2023-02-21 02:06:35 -0500

    hello

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -28,11 +28,32 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 window.exitonclick()
 
-
 import pygame
 import math
 
 pygame.init()
-window = pygame.display.set_mode()
 
+window = pygame.display.set_mode()
+points = []
+num_side = [3,4,6,20,100,360]
+color = ["brown", "cadetblue", "cyan", "gold", "darkolivegreen", "darkred"]
+side_length = 150
+
+xpos = 725
+ypos = 400
+
+for s in range(len(num_side)):
+    for i in range(num_side[s]):
+        angle = 360/num_side[s]
+        radians = math.radians(angle*i)
+        x = xpos + side_length*math.cos(radians)
+        y = ypos + side_length*math.sin(radians)
+        points.append([x,y])
+    pygame.draw.polygon(window, color[s], points)
+    pygame.display.flip()
+    pygame.time.wait(1000)
+    window.fill("burlywood")
+    pygame.display.flip()
+    points = []
+    
 window.exitonclick()
