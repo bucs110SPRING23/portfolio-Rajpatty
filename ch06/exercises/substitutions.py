98330712a37fc5dc923ed82751e9042d420db7dd
syamrajpatty <srajpat1@binghamton.edu>
2023-03-22 22:17:11 -0400

    hello

---
+++ ch06/exercises/substitutions.py
@@ -0,0 +1,19 @@
+import json
+
+def main():
+    text = open("ch06/exercises/news.txt","r").read().lower()
+    sub_fptr = open("ch06/exercises/subs.json","r")
+    subs = json.load(sub_fptr)
+
+    print(subs,type(subs))
+
+    for k,v in subs.items():
+        text = text.replace(k,v)
+
+    fptr = open("ch06/exercises/betternews.txt","w")
+    fptr.write(text)
+    fptr.close()
+
+
+
+main()
